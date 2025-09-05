6aeb964d841b597b6114a127557dc2f1570b89c3
sanjeevk2002 <sanjeevpaswan2002@gmail.com>
2025-09-06 02:55:57 +0530

    promise file

---
+++ promise.js
@@ -0,0 +1,37 @@
+const responce = new Promise((resolve ,reject)=>{
+    let name = "Sanjeev";
+    console.log(name);
+    resolve(name);
+})
+
+responce.then((user)=>{
+    console.log(user);
+})
+.catch((error)=>{
+    console.log("error");
+})
+
+
+const responce2 = new Promise((resolve ,reject)=>{
+    setTimeout(()=>{
+        let error = false;
+        if(!error){
+            console.log("your promise has been resolved");
+            resolve({name : "sanjeev" , age : 23 , rollno : 101});
+        }else{
+            console.log("your promise has not been resolved");
+            reject("sorry not fulfilled");
+        }
+    },1000);
+});
+responce2
+.then((user)=>{
+    console.log(user);
+    return user.name;
+})
+.then((name)=>{
+    console.log(name);
+})
+.catch((error)=>{
+    console.log("error" ,error);
+})
