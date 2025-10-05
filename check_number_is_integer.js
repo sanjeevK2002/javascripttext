8c15edf5d8771dd083dceede3bbf05c4951d2f51
sanjeevk2002 <sanjeevpaswan2002@gmail.com>
2025-10-05 15:54:35 +0530

    Implement integer check function and test with a sample number

--- check_number_is_integer.js
+++ check_number_is_integer.js
@@ -1,2 +1,11 @@
 // check a number is interger
 
+const checkInteger =(num)=>{
+   if(num % 1 === 0 )
+    return `${num} is a integer`
+else
+    return `${num} is not interger`
+}
+const num = -5;
+const res=  checkInteger(num);
+console.log(res);
